4a2a9f6f471fdb8cc1bc72f8498a883a5805a866
arran <lomasarran1993@googlemail.com>
2017-10-17 21:11:31 +1000

    add navigation drawer

--- app/src/main/java/com/arran/askaquestion/views/main/mvp/MainActivity.kt
+++ app/src/main/java/com/arran/askaquestion/views/main/mvp/MainActivity.kt
@@ -6,12 +6,19 @@ import com.arran.askaquestion.R
 import com.arran.askaquestion.views.base.BaseActivity
 import com.arran.askaquestion.views.main.MainActivityModule
 import com.arran.askaquestion.views.question.QuestionsFragment
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+
 
 class MainActivity : BaseActivity(), MainContract.View {
 
     @Inject
     lateinit var presenter: MainContract.Presenter
+
+    private lateinit var nawDrawer: Drawer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +26,25 @@ class MainActivity : BaseActivity(), MainContract.View {
         AskAQuestion.presenterComponent.add(MainActivityModule()).inject(this)
         presenter.attachView(this)
         refreshFragment()
+        setupNavigationDrawer()
     }
 
-    private fun refreshFragment(){
+    fun setupNavigationDrawer() {
+        val item2 = SecondaryDrawerItem().withIdentifier(2).withName("channels")
+        nawDrawer = DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(main_toolbar)
+                .addDrawerItems(
+                        item2,
+                        SecondaryDrawerItem().withName("channel 1")
+                )
+                .withOnDrawerItemClickListener { view, position, drawerItem ->
+                    true
+                }
+                .build()
+    }
+
+    private fun refreshFragment() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.main_content, QuestionsFragment.newInstance())
         fragmentTransaction.commit()
